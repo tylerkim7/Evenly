a2b9f3eb1e171c8d47ea1d3df22517c08db768e1
x <xbook@Mac.attlocal.net>
2025-06-07 10:56:40 -0700

    Added prelim express backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const upload = multer({ 
+    storage: multer.memoryStorage()
+});
+
+app.post ('/receipt', upload.single('receiptImg'), async(req, res) => {
+try {
+    console.log('Image Received:')
+    res.json({
+      success: true,
+      message: 'Image processed successfully'
+    });
+} catch (error) {
+    console.error('Error:', error);
+}});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
