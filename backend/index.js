316d97ade81388409fce0193a58184e6a5c039ad
x <xbook@Mac.attlocal.net>
2025-06-08 10:14:58 -0700

    Checking to see if my commits are now signed

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,16 @@ const multer = require('multer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// using mutler, a npm package for middleware functionality and uploading files
 const upload = multer({ 
     storage: multer.memoryStorage()
 });
 
 app.post ('/receipt', upload.single('receiptImg'), async(req, res) => {
 try {
-    console.log('Image Received:')
     res.json({
       success: true,
-      message: 'Image processed successfully',
+      message: 'Image received successfully',
       imageInfo: {
         size: req.file.size,
         type: req.file.mimetype,
