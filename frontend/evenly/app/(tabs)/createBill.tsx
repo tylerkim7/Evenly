2043669e8de232f58076029dd069aa50cd2f2a48
Tyler Kim <tylerkim.nm@gmail.com>
2025-07-10 14:15:04 -0700

    added tabs/bottom tab bar

---
+++ frontend/evenly/app/(tabs)/createBill.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function CreateBillScreen() {
+  return (
+    <View>
+      <Text>Create a Bill</Text>
+    </View>
+  );
+}
---
+++ frontend/evenly/app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+// app/(tabs)/_layout.tsx
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="createBill"
+        options={{
+          title: "Create Bill",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cloud-upload-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
